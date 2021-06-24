f5b31d6f518c9b1af6d8b317ede3bbdf38f7dd20
Vinícius Melo <ViniciusCMelo@users.noreply.github.com>
2021-06-24 16:09:18 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+const admin = require("firebase-admin");
+require('dotenv').config();
+
+const serviceAccount = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+
+const app = admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://flud-reactnative-default-rtdb.firebaseio.com"
+});
+const db = app.firestore();
+
+db.collection("users").doc("lragozzine").set({
+    first: 'Vinicius',
+    last: 'Ragozzine',
+    address: '133 5th St., San Francisco, CA',
+    birthday: '05/13/1990',
+    age: '30',
+});
+
